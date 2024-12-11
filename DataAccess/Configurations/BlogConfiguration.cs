a425946b973e28587743f2824a9e48d35d722b8d
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2024-12-11 11:10:48 +0100

    Blog Configuration Added

---
+++ DataAccess/Configurations/BlogConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Core.DefaultValues;
+using Entities.TableModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.ToTable("Blogs");
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(DefaultConstantValues.DEFAULT_PRIMARY_SEED_VALUE, 1);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            builder.Property(x => x.Text)
+                .IsRequired()
+                .HasMaxLength(5000);
+
+            builder.Property(x => x.PhotoUrl)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
