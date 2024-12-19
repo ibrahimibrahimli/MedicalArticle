9be56d928760602e428f11662326bae16cead9ae
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2024-12-19 20:48:13 +0100

    AboutController added

---
+++ MedicalArticles/Controllers/AboutController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using MedicalArticles.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalArticles.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IServiceService _serviceService;
+        private readonly IWhyChooseUsService _whyUsService;
+        private readonly ITeamBoardService _teeamBoardService;
+
+        public AboutController(IServiceService serviceService, IWhyChooseUsService whyUsService, ITeamBoardService teeamBoardService)
+        {
+            _serviceService = serviceService;
+            _whyUsService = whyUsService;
+            _teeamBoardService = teeamBoardService;
+        }
+
+        public IActionResult Index()
+        {
+            var currentLanguage = Thread.CurrentThread.CurrentCulture.Name;
+
+            var serviceData = _serviceService.GetDataByLanguage(currentLanguage).Data;
+            var whyUsData = _whyUsService.GetWhyUsWithItems(currentLanguage).Data;
+            var teamboardData = _teeamBoardService.GetDataByLanguage(currentLanguage).Data;
+
+            AboutViewModel aboutData = new AboutViewModel()
+            {
+                Services = serviceData,
+                WhyChooseUS = whyUsData,
+                Teamboards = teamboardData
+            };
+            return View();
+        }
+    }
+}
--- MedicalArticles/ViewModels/AboutViewModel.cs
+++ MedicalArticles/ViewModels/AboutViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace MedicalArticles.ViewModels
+﻿using Entities.Dtos;
+using Entities.TableModels;
+
+namespace MedicalArticles.ViewModels
 {
     public class AboutViewModel
     {
+        public List<Service> Services { get; set; }
+        public List<WhyChooseUsDto> WhyChooseUS { get; set; }
+        public List<TeamBoard> Teamboards { get; set; }
     }
 }
