76a0eb4458e87e9f16252db97966eee7bbdc6e70
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2024-12-20 15:18:51 +0100

    TeamController added

---
+++ MedicalArticles/Controllers/HealthTipController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalArticles.Controllers
+{
+    public class HealthTipController : Controller
+    {
+        private readonly IHealtTipService? _healthTipService;
+
+        public HealthTipController(IHealtTipService? healthTipService)
+        {
+            _healthTipService = healthTipService;
+        }
+
+        public IActionResult Index()
+        {
+            var currentLanguage = Thread.CurrentThread.CurrentCulture.Name;
+            var data = _healthTipService.GetHealtTipsWithItems(currentLanguage).Data;
+            return View(data);
+        }
+    }
+}
---
+++ MedicalArticles/Controllers/TeamController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalArticles.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ITeamBoardService? _teamBoardService;
+
+        public TeamController(ITeamBoardService? teamBoardService)
+        {
+            _teamBoardService = teamBoardService;
+        }
+
+        public IActionResult Index()
+        {
+            var currentLanguage = Thread.CurrentThread.CurrentCulture.Name;
+            var data = _teamBoardService.GetDataByLanguage(currentLanguage).Data;
+            return View(data);
+        }
+    }
+}
