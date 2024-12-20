920087368d9ee3bd29705d7d6a1f81e133cace33
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2024-12-20 15:41:14 +0100

    ContactController updated

--- MedicalArticles/Controllers/ContactController.cs
+++ MedicalArticles/Controllers/ContactController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalArticles.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IContactService _contactService;
+        private readonly IAdressService _adressService;
+
+        public ContactController(IContactService contactService, IAdressService adressService)
+        {
+            _contactService = contactService;
+            _adressService = adressService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var currentLanguage = Thread.CurrentThread.CurrentCulture.Name;
+            var data = _adressService.GetDataByLanguage(currentLanguage).Data;
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult Create( ContactCreateDto dto)
+        {
+            _contactService.Add(dto);
+            return RedirectToAction("Index");
         }
     }
 }
