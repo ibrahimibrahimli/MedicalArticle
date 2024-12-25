68fcd1330dab17663c459768099944aacfcdef6a
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2024-12-25 19:34:59 +0100

    Blog ViewModel added

---
+++ MedicalArticles/ViewModels/BlogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Business.Abstract;
+using Entities.TableModels;
+
+namespace MedicalArticles.ViewModels
+{
+    public class BlogViewModel
+    {
+        public List<Blog> Blogs { get; set; }
+        public List<Sosial> Sosials { get; set; }
+    }
+}
