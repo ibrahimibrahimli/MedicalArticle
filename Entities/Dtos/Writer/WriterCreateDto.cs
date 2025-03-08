57f3dc6385b23fc2ac4b82e3ee75568556af21f0
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-03-08 22:36:48 +0100

    Refactor Writer DTOs and update DocumentLayout.json
    - Updated DocumentLayout.json:
      - Removed references to WriterDal.cs and AdressDal.cs.
      - Added references to WriterDto.cs and WriterUpdateDto.cs.
      - Removed Comment.cs entry.
      - Updated Writer.cs entry with new ViewState and WhenOpened timestamp.
      - Added WriterUpdateDto.cs and WriterDto.cs entries with details.
    - Added WriterCreateDto.cs in Entities.Dtos namespace:
      - Properties: Name, Surname.
      - Static method: ToWriter.

---
+++ Entities/Dtos/Writer/WriterCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using Entities.TableModels;
+
+namespace Entities.Dtos
+{
+    public class WriterCreateDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public static Writer ToWriter(WriterCreateDto dto)
+        {
+            return new()
+            {
+                Name = dto.Name,
+                Surname = dto.Surname,
+            };
+        }
+    }
+}
---
+++ Entities/Dtos/Writer/WriterUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using Entities.TableModels;
+
+namespace Entities.Dtos
+{
+    public class WriterUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public static Writer ToWriter(WriterUpdateDto dto)
+        {
+            return new()
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Surname = dto.Surname,
+            };
+        }
+    }
+}
